36ba51bed86669dcf37907f2ee4d7ab09d270487
Wesley <simpletextbr>
2021-08-07 00:35:33 -0300

    Routes Updates

--- src/components/title-pages/index.tsx
+++ src/components/title-pages/index.tsx
@@ -13,7 +13,7 @@ function TitlePages ({ title }:ITitle):JSX.Element{
   return (
     <S.Title>
       <div className="link">
-        <Link to="/wesleypraca"><p className="home">HOME<FiChevronRight size={16} /></p> </Link>
+        <Link to="/"><p className="home">HOME<FiChevronRight size={16} /></p> </Link>
         <p>{title.toUpperCase()} <FiChevronRight size={16} color="#727272" /></p>
       </div>
       <p>{title}</p>
